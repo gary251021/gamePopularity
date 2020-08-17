c2f6ad1657618cd81fa056655dd742772658d831
worstit9 <56362276+worstit9@users.noreply.github.com>
2020-08-18 00:56:35 +0800

    finish finding population and new thread

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import urllib.request as r
 import traceback
 import time
+import re
+import unicodedata
 
 class population:
     
@@ -19,10 +21,11 @@ class population:
     def usingDriver(self,url):
         driver = webdriver.Chrome('chromedriver.exe')
         driver.get(url)
-        time.sleep(1)
+        time.sleep(0.5)
         return driver
     def getSoup(self,url):
         #return bs4 object of this website
+        #using selenium
         try:
             d = self.usingDriver(url)
             soup = bs4.BeautifulSoup(d.page_source,"html.parser")
@@ -33,12 +36,38 @@ class population:
             #print(f"\n{tb}")
             print(Exception)
             return 0
-    def getData(self,url):
-        #return python dictionary
-        #grap data every ? minutes
+    def getFormDiv(self,url):
+        #get <div id=data-container>
         soup = self.getSoup(url)
         form = soup.find(id="data-container")
-        print(form)
+        return form
+    def getData(self,url):
+        #return list of dictionary
+        #{"gameName":str,"gameId":int,"rank":int,"population":int,"newThread":int}
+        #grap data every ? minutes
+        numRegex = "[\s]\d+[\s]"
+        storage = []
+        form = self.getFormDiv(url)
+        for i in form.find_all(class_="forum_list"):
+            data = {}
+            print(i.find(class_="forum_list_title").find_all("span")) #rank,gameId,gameName
+            
+            """txt = self.getPopulationAndThread(i)
+            data["population"] = txt[0]
+            data["newThread"] = txt[1]"""
+            #print(data)
+
+    def getPopulationAndThread(self,iter):
+        numRegex = "[\s]\d+[\s]"
+        txt = re.findall(numRegex,iter.getText())
+        txt = self.strtoInt(txt)
+        return txt
+
+    def strtoInt(self,li):
+        li = [unicodedata.normalize("NFKC",line) for line in li] #change \u3000 to std space
+        for i in range(len(li)):
+            li[i] = int(li[i])
+        return li
 
     def checkIdentical(self,fileName,dict):
         #check whether the json file should be updated
@@ -71,3 +100,4 @@ time.sleep(1)
 soup = bs4.BeautifulSoup(driver.page_source)"""
 p = population()
 p.getData(p.onlineLink)
+
