e8e079435db32e7e3c5eaf47242beff97c5c99bf
worstit9 <56362276+worstit9@users.noreply.github.com>
2020-08-17 23:27:34 +0800

    adding selenium

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from selenium import webdriver
 import bs4
 import json
 import urllib.request as r
 import traceback
+import time
+
 class population:
+    
+
     onlineLink = "https://forum.gamer.com.tw/index.php?c=400"
     mobileLink = "https://forum.gamer.com.tw/index.php?c=94"
     webGameLink = "https://forum.gamer.com.tw/index.php?c=80"
@@ -10,13 +15,30 @@ class population:
     consoleLink = "https://forum.gamer.com.tw/index.php?c=52"
     def __init__(self):
         pass
-    def mainLoop(self):
-        pass
-
+    
+    def usingDriver(self,url):
+        driver = webdriver.Chrome('chromedriver.exe')
+        driver.get(url)
+        time.sleep(1)
+        return driver
+    def getSoup(self,url):
+        #return bs4 object of this website
+        try:
+            d = self.usingDriver(url)
+            soup = bs4.BeautifulSoup(d.page_source,"html.parser")
+            return soup
+        except Exception:
+            print("some error is occured in getSoup function")
+            #tb = traceback.format_exc()
+            #print(f"\n{tb}")
+            print(Exception)
+            return 0
     def getData(self,url):
         #return python dictionary
-        #grap data every 240 minutes
-        pass
+        #grap data every ? minutes
+        soup = self.getSoup(url)
+        form = soup.find(id="data-container")
+        print(form)
 
     def checkIdentical(self,fileName,dict):
         #check whether the json file should be updated
@@ -39,6 +61,13 @@ class population:
             print(f"\n{tb}")
         else:
             print("Finished")
+    def mainLoop(self):
+        pass
 
+"""url = "https://worstit9.github.io/index.html"
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get(url)
+time.sleep(1)
+soup = bs4.BeautifulSoup(driver.page_source)"""
 p = population()
-p.printInfo(p.onlineLink)
+p.getData(p.onlineLink)
