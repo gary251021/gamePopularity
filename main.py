1620643ba6450fffd7a9d4e6059daf1695f47840
worstit9 <56362276+worstit9@users.noreply.github.com>
2020-08-20 18:13:13 +0800

    data of 0820

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ class population:
             try:
                 with open(f"data/{fileName}","w",encoding="utf-8") as f:
                     json.dump(data,f,ensure_ascii=False)
-                    print("Successfully updated")
+                    print(f"Successfully updated {fileName}")
             except Exception:
                 print(Exception)
     #debug
